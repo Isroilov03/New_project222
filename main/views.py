78940ff2c87452d6227969ef05bb2da8f37674e3
Azizbek <pythondeveloper611@gmail.com>
2023-09-09 11:15:04 +0500

    change

--- main/views.py
+++ main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
 def index_view(request):
-    return render(request, 'index.html')
+    context = {
+        "talabalar" : Client.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
+print("salom")
+print("salom")
+print("salom")
+print("salom")
